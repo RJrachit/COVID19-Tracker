dbe54ad951316861f88f3eae3cadb882d51e9813
RJrachit <51331327+RJrachit@users.noreply.github.com>
2020-04-19 00:23:54 +0530

    Major Changes
    Graph added. Delta changes added.
    Color and font scheme changed.

--- table.js
+++ table.js
@@ -12,10 +12,6 @@ function sortByProperty(property){
    }
 }
 
-$('.act').on('click',function(){
-  sortByProperty('active');
-  write2(st);
-})
 function write2(st)
 {
   for(var i=0;i<st.length;i++)
@@ -25,7 +21,13 @@ function write2(st)
   }
   var row = $('tr');
   for(var i=1;i<row.length;i++){
-    row.eq(i).find("td").eq(0).addClass("one");
+    var cell = row.eq(i).find("td");
+    cell.eq(0).addClass("one");
+    var active = parseInt(st[i-1].deltaconfirmed) - parseInt(st[i-1].deltarecovered) - parseInt(st[i-1].deltadeaths);
+    cell.eq(1).append("<br> <span class = \"con \"> <i class = \"fa fa-caret-up \"> </i>" +st[i-1].deltaconfirmed + "</span>");
+    cell.eq(2).append("<br> <span class = \"act \"> <i class = \"fa fa-caret-up \"> </i>" +active + "</span>");
+    cell.eq(3).append("<br> <span class = \"rec \"> <i class = \"fa fa-caret-up \"> </i>" +st[i-1].deltarecovered + "</span>");
+    cell.eq(4).append("<br> <span class = \"dec \"> <i class = \"fa fa-caret-up \"> </i>" +st[i-1].deltadeaths + "</span>");
   }
 }
 
