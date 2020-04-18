d855632a711e699ae890dbc133cf1373913a518c
RJrachit <51331327+RJrachit@users.noreply.github.com>
2020-04-19 02:43:30 +0530

    Sorting added

--- table.js
+++ table.js
@@ -24,8 +24,8 @@ function write2(st)
     var cell = row.eq(i+1).find("td");
     cell.eq(0).addClass("one");
     console.log(st);
-    var acti = parseInt(st[i].deltaconfirmed) - parseInt(st[i].deltarecovered) - parseInt(st[i].deltadeaths);
-    cell.eq(1).append("<br> <span class = \"con \"> <i class = \"fa fa-caret-up \"> </i>" +st[i].deltaconfirmed + "</span>");
+    var acti = parseInt( st[i].deltaconfirmed ) - parseInt(st[i].deltarecovered) - parseInt(st[i].deltadeaths);
+    cell.eq(1).append("<br> <span class = \"con \"> <i class = \"fa fa-caret-up \"> </i>" + st[i].deltaconfirmed + "</span>");
     cell.eq(2).append("<br> <span class = \"act \"> <i class = \"fa fa-caret-up \"> </i>" +acti + "</span>");
     cell.eq(3).append("<br> <span class = \"rec \"> <i class = \"fa fa-caret-up \"> </i>" +st[i].deltarecovered + "</span>");
     cell.eq(4).append("<br> <span class = \"dec \"> <i class = \"fa fa-caret-up \"> </i>" +st[i].deltadeaths + "</span>");
@@ -45,8 +45,34 @@ function changeOrder(prop)
     write2(st);
   })
 }
-
-
+$('.dec').on('click',function(){
+  var row  = $('tr');
+  for(var i=1;i<row.length;i++){
+    row[i].remove();
+  }
+  changeOrder("deaths");
+})
+$('.rec').on('click',function(){
+  var row  = $('tr');
+  for(var i=1;i<row.length;i++){
+    row[i].remove();
+  }
+  changeOrder("recovered");
+})
+$('.act').on('click',function(){
+  var row  = $('tr');
+  for(var i=1;i<row.length;i++){
+    row[i].remove();
+  }
+  changeOrder("active");
+})
+$('.con').on('click',function(){
+  var row  = $('tr');
+  for(var i=1;i<row.length;i++){
+    row[i].remove();
+  }
+  changeOrder("confirmed");
+})
 $.getJSON("https://api.covid19india.org/data.json",function(data){
   // console.log(data);
   console.log(data.statewise);
@@ -58,8 +84,7 @@ $.getJSON("https://api.covid19india.org/data.json",function(data){
   $('.deaths').text(states[0].deaths);
 
   var st = states;
-  st.sort(sortByProperty("confirmed"));
+  changeOrder("active");
   console.log(st);
 
-  write2(st);
 })
